33a07fd9f9389e16b1db8e296995f73e326bfbca
kazurayam <kazuaki.urayama@gmail.com>
2023-01-27 14:50:32 +0900

    update the chromedriverfactory jar which supports the pref that grants access to the clipboard

--- Scripts/readClipboard/Script1674635624363.groovy
+++ Scripts/readClipboard/Script1674635624363.groovy
@@ -1,7 +1,11 @@
 import com.kms.katalon.core.testobject.ConditionType
 import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-
+import com.kazurayam.webdriverfactory.chrome.ChromeDriverFactory;
+import com.kazurayam.webdriverfactory.chrome.LaunchedChromeDriver;
+import org.openqa.selenium.WebDriver
+import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kazurayam.webdriverfactory.chrome.ChromePreferencesModifiers
 TestObject makeTestObject(String xpath) {
 	TestObject tObj = new TestObject(xpath)
 	tObj.addProperty("xpath", ConditionType.EQUALS, xpath)
@@ -12,19 +16,30 @@ String url = "https://codepen.io/RevCred/pen/vxXrww"
 TestObject locatorIFrame = makeTestObject("//iframe[@id='result']")
 TestObject locatorButton = makeTestObject("//div[@id='copy']")
 
-WebUI.openBrowser("")
+//WebUI.openBrowser("")
+
+System.setProperty("webdriver.chrome.driver", DriverFactory.getChromeDriverPath())
+ChromeDriverFactory cdFactory = ChromeDriverFactory.newChromeDriverFactory()
+// grant JavaScript to get access to the OS Clipboard 
+cdFactory.addChromePreferencesModifier(ChromePreferencesModifiers.grantAccessToClipboard())
+LaunchedChromeDriver launched = cdFactory.newChromeDriver()
+WebDriver driver = launched.getDriver()
+DriverFactory.changeWebDriver(driver)
+
 WebUI.navigateToUrl(url)
 WebUI.verifyElementPresent(locatorIFrame, 10)
 WebUI.switchToFrame(locatorIFrame, 10)
 WebUI.verifyElementPresent(locatorButton, 10)
 WebUI.click(locatorButton)
 
+// execute a JavaScript script in the Chrome browser, which will read a text from the clipboard and return it to the caller
 String js = """
     return navigator.clipboard.readText();
 """
-
 String text = WebUI.executeJavaScript(js, null)
-WebUI.comment("text copied from the clipboard: " + text)
+
+WebUI.comment("text read from the clipboard: " + text)
+assert text.startsWith("https://staging.revolutioncredit.com/signupc/")
 
 WebUI.closeBrowser()
 
